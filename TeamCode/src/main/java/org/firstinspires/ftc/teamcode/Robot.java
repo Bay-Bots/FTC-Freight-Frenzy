e4ebffdd1920f3933c7e06888c7c0346143dfcd3
jonahgluck <61380701+jonahgluck@users.noreply.github.com>
2021-09-19 12:07:28 -0400

    global robotics class and driver control implementation for testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Robot {
+    private DcMotor motorFrontRight;
+    private DcMotor motorFrontLeft;
+    private DcMotor motorBackRight;
+    private DcMotor motorBackLeft;
+
+    public Robot() {
+        motorFrontRight = hardwareMap.dcMotor.get("motor front right");
+        motorFrontLeft = hardwareMap.dcMotor.get("motor front left");
+        motorBackLeft = hardwareMap.dcMotor.get("motor back left");
+        motorBackRight = hardwareMap.dcMotor.get("motor back right");
+        // initialization is complete
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // in autonomous call as well
+        // waitForStart();
+    }
+
+    public void setDriverMotorPower(float FRightPower, float FLeftPower, float BRightPower, float BLeftPower) {
+        motorFrontRight.setPower(FRightPower);
+        motorFrontLeft.setPower(FLeftPower);
+        motorBackLeft.setPower(BLeftPower);
+        motorBackRight.setPower(BRightPower);
+    }
+}
