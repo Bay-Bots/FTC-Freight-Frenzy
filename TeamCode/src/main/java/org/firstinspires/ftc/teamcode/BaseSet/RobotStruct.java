8889f8798a632f22ecbde858795447689b87fe1e
Jonah <61380701+jonahgluck@users.noreply.github.com>
2021-10-08 17:45:20 -0400

    fixed #10 but don't know if it works, added motor to spin the duck, added 2 arm motors, added servo for gripping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseSet/RobotStruct.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseSet/RobotStruct.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.BaseSet;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
+import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 public class RobotStruct extends OpMode {
@@ -10,7 +10,10 @@ public class RobotStruct extends OpMode {
     DcMotor motorFrontLeft;
     DcMotor motorBackRight;
     DcMotor motorBackLeft;
+    DcMotor motorDuckDropper;
     DcMotorEx motorArm;
+    DcMotorEx motorArmDuo;
+    Servo servoClaw;
 
     @Override
     public void init() {
@@ -18,7 +21,10 @@ public class RobotStruct extends OpMode {
         motorFrontLeft = hardwareMap.get(DcMotor.class, "motor front left");
         motorBackLeft = hardwareMap.get(DcMotor.class, "motor back left");
         motorBackRight = hardwareMap.get(DcMotor.class, "motor back right");
+        motorDuckDropper = hardwareMap.get(DcMotor.class, "motor duck dropper");
         motorArm = hardwareMap.get(DcMotorEx.class, "motor arm");
+        motorArmDuo = hardwareMap.get(DcMotorEx.class, "motor arm duo");
+        servoClaw = hardwareMap.get(Servo.class, "servo claw");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -36,12 +42,21 @@ public class RobotStruct extends OpMode {
         motorBackRight.setPower(BRightPower);
     }
 
+    public void setDuckDropperSpeed(double speed){
+        motorDuckDropper.setPower(speed);
+    }
+
     public void setArmSpeed(double speed) {
         motorArm.setPower(speed);
+        motorArmDuo.setPower(speed);
     }
 
     public void sendAmpReading() {
         telemetry.addData("Arm Current: ", motorArm.getCurrent(CurrentUnit.AMPS));
         telemetry.update();
+    }
+
+    public void setClawPos(double pos) {
+        servoClaw.setPosition(pos);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -10,13 +10,19 @@ public class DriverControl extends RobotStruct {
         double speed = gamepad1.right_trigger - gamepad1.left_trigger;
         double turn = gamepad1.left_stick_x;
         double armSpeed = gamepad1.right_stick_y;
+        double duckDropperSpeed = gamepad2.right_trigger - gamepad2.left_trigger;
 
         setDriverMotorPower(speed - turn, speed + turn, speed - turn, speed + turn);
+        setDuckDropperSpeed(duckDropperSpeed);
         setArmSpeed(armSpeed);
+        sendAmpReading();
 
-        if(gamepad1.x){
-            // get the current (in amps) from the arm motor
-            sendAmpReading();
+        if(gamepad2.a) {
+            setClawPos(0.30);
+        }
+
+        if(gamepad2.y) {
+            setClawPos(0.50);
         }
     }
 }
